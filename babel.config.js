563d807559fa18911b6a3a1854325e526e5a004f
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-09-08 00:10:10 +0200

    Configurando o babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
-preinitModule.exports = {
-    presents: [
-        '@babel/preset-env' 
-    ] 
-    
-}
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react' 
+    ]     
+};
