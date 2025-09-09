481d37ac9a743fbdfa6972ffee193e860c576e24
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-09-09 17:30:00 +0200

    First Component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,13 @@
+export function RepositoryList() {
+    return (
+        <section>
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li><strong>Unform</strong></li>
+                <p>Forms in React</p>
+                <a href="#">acessar</a>
+            </ul>
+        </section>
+    );
+}
